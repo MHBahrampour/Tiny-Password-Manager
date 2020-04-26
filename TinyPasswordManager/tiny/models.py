5e17e11d81a43eab9ab132b867b684dd9b382efb
bpmmd <bahrampour@pm.me>
2020-04-26 22:18:07 +0430

    Model PasswordInstance Created

--- TinyPasswordManager/tiny/models.py
+++ TinyPasswordManager/tiny/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class PasswordInstance(models.Model):
+    # username
+    user = models.CharField(max_length=200)
+    # password title
+    title = models.CharField(max_length=200)
+    # password description
+    description = models.TextField(max_length=2000)
+
+    # password 
+    password = models.CharField(max_length=32, blank=True)
+    # or
+    #user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.title
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular instance of the model."""
+        return reverse('pass-detail', args=[str(self.id)])
