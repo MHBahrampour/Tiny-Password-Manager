3533279c112f51ea682f0f2f41ea19ee0d03d58d
bpmmd <bahrampour@pm.me>
2020-05-18 18:06:39 +0430

    Implement signup page

--- TinyPasswordManager/TinyPasswordManager/urls.py
+++ TinyPasswordManager/TinyPasswordManager/urls.py
@@ -2,11 +2,22 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from django.views.generic import RedirectView
+from django.conf.urls import url
+from tiny import views as core_views
 
 urlpatterns = [
+    # admin site
     path('admin/', admin.site.urls),
+
     # include urls.py from tiny application
     path('tiny/', include('tiny.urls')),
+
     # add URL maps to redirect the base URL to tiny application
     path('', RedirectView.as_view(url='tiny/', permanent=True)),
+
+    # for signin
+    path('signin/', include('django.contrib.auth.urls')),
+
+    # for signup
+    url(r'^signup/$', core_views.signup, name='signup'),
 ]
--- TinyPasswordManager/tiny/views.py
+++ TinyPasswordManager/tiny/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from tiny.models import PasswordInstance
 
 from django.contrib.auth.decorators import login_required
+
 @login_required
 def index(request):
     """View function for home page of site."""
@@ -13,3 +14,17 @@ def index(request):
         'wellcome': 'Wellcome "{}" .'.format(username),
     }
     return render(request, 'index.html', context=context)
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'tinyapp/signup.html', {'form': form})
